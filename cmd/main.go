cd1d49d8e653f3380578ccba7b1479bedcb18019
Guilherme Costa Sales <guilhermecosales@gmail.com>
2025-08-04 23:41:10 -0300

    feat: retrieve environment variables from .env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"github.com/guilhermecosales/security-service/config"
 	"github.com/guilhermecosales/security-service/internal/server"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	srv := server.New(":8080")
+	env, err := config.LoadEnvironmentVariables()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to load environment variables")
+	}
 
-	log.Info().Msgf("Starting server at %s", srv.Addr)
+	srv := server.New(":" + env.ApplicationPort)
+
+	log.Info().Msgf("Starting '%s' in '%s' mode on port :%s", env.ApplicationName, env.Environment, env.ApplicationPort)
 	log.Fatal().Err(srv.ListenAndServe())
 }
---
+++ config/env.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	_ "github.com/joho/godotenv/autoload"
+	"os"
+)
+
+type Config struct {
+	Environment     string
+	ApplicationName string
+	ApplicationPort string
+	DatabaseConfig  DatabaseConfig
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+}
+
+func LoadEnvironmentVariables() (*Config, error) {
+	cfg := &Config{
+		Environment:     getEnv("ENVIRONMENT", "production"),
+		ApplicationName: getEnv("APP_NAME", "security-service"),
+		ApplicationPort: getEnv("APPLICATION_PORT", "3001"),
+		DatabaseConfig: DatabaseConfig{
+			Host:     getEnv("DATABASE_HOST", "localhost"),
+			Port:     getEnv("DATABASE_PORT", "5432"),
+			Username: getEnv("DATABASE_USER", "postgres"),
+			Password: getEnv("DATABASE_PASSWORD", "postgres"),
+		},
+	}
+
+	return cfg, nil
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
